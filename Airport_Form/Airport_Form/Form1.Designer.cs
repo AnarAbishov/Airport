5076368ab9b51f73aeebeaa673bcdbdfb5a2b73b
AnarAbishov <mr.avinar@mail.ru>
2017-11-06 18:09:33 +0400

    Add files via upload

---
+++ Airport_Form/Airport_Form/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Airport_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvFlight = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Type = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cmbxRunway = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbxGate = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbxCity = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbxCountry = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbxCompany = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFlight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFlight
+            // 
+            this.dgvFlight.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFlight.Location = new System.Drawing.Point(13, 297);
+            this.dgvFlight.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvFlight.Name = "dgvFlight";
+            this.dgvFlight.Size = new System.Drawing.Size(1063, 232);
+            this.dgvFlight.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(519, 123);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 17);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Data";
+            // 
+            // Type
+            // 
+            this.Type.AutoSize = true;
+            this.Type.Location = new System.Drawing.Point(287, 21);
+            this.Type.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(40, 17);
+            this.Type.TabIndex = 31;
+            this.Type.Text = "Type";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(507, 159);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 30;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(445, 52);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(93, 21);
+            this.radioButton2.TabIndex = 29;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Departure";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(283, 52);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 21);
+            this.radioButton1.TabIndex = 28;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Arrival";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(507, 243);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(191, 28);
+            this.btnAdd.TabIndex = 27;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // cmbxRunway
+            // 
+            this.cmbxRunway.FormattingEnabled = true;
+            this.cmbxRunway.Location = new System.Drawing.Point(283, 245);
+            this.cmbxRunway.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbxRunway.Name = "cmbxRunway";
+            this.cmbxRunway.Size = new System.Drawing.Size(160, 24);
+            this.cmbxRunway.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 209);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Runway";
+            // 
+            // cmbxGate
+            // 
+            this.cmbxGate.FormattingEnabled = true;
+            this.cmbxGate.Location = new System.Drawing.Point(283, 159);
+            this.cmbxGate.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbxGate.Name = "cmbxGate";
+            this.cmbxGate.Size = new System.Drawing.Size(160, 24);
+            this.cmbxGate.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(283, 123);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Gate";
+            // 
+            // cmbxCity
+            // 
+            this.cmbxCity.FormattingEnabled = true;
+            this.cmbxCity.Location = new System.Drawing.Point(22, 245);
+            this.cmbxCity.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbxCity.Name = "cmbxCity";
+            this.cmbxCity.Size = new System.Drawing.Size(160, 24);
+            this.cmbxCity.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 209);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 17);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "City";
+            // 
+            // cmbxCountry
+            // 
+            this.cmbxCountry.FormattingEnabled = true;
+            this.cmbxCountry.Location = new System.Drawing.Point(22, 159);
+            this.cmbxCountry.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbxCountry.Name = "cmbxCountry";
+            this.cmbxCountry.Size = new System.Drawing.Size(160, 24);
+            this.cmbxCountry.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Country";
+            // 
+            // cmbxCompany
+            // 
+            this.cmbxCompany.FormattingEnabled = true;
+            this.cmbxCompany.Location = new System.Drawing.Point(22, 52);
+            this.cmbxCompany.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbxCompany.Name = "cmbxCompany";
+            this.cmbxCompany.Size = new System.Drawing.Size(160, 24);
+            this.cmbxCompany.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Company";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 525);
+            this.Controls.Add(this.dgvFlight);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Type);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cmbxRunway);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbxGate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbxCity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbxCountry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbxCompany);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFlight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFlight;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Type;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox cmbxRunway;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbxGate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbxCity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbxCountry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbxCompany;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Airport_Form/Airport_Form/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Airport_Form.Models;
+
+
+namespace Airport_Form
+{
+    public partial class Form1 : Form
+    {
+        private AirportEntities db = new AirportEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadData();
+            fillCompany();
+            fillCountry();
+            fillCity();
+            fillGate();
+            fillRunway();
+        }
+
+        private void loadData()
+        {
+            dgvFlight.DataSource = db.Flights.Select(f => new { f.id, f.CompanyId, f.Cities.CountryId, f.CityId, f.GateId, f.RunwayId }).ToList();
+        }
+
+        private void fillCompany()
+        {
+            List<Companies> company = db.Companies.ToList();
+            foreach(Companies item in company)
+            {
+                cmbxCompany.Items.Add(item.Name);
+            }
+        }
+        private void fillCountry()
+        {
+            List<Countries> country = db.Countries.ToList();
+            foreach (Countries item in country)
+            {
+                cmbxCountry.Items.Add(item.Name);
+            }
+        }
+        private void fillCity()
+        {
+            List<Cities> city = db.Cities.ToList();
+            foreach (Cities item in city)
+            {
+                cmbxCity.Items.Add(item.Name);
+            }
+        }
+        private void fillGate()
+        {
+            List<Gates> gate = db.Gates.ToList();
+            foreach (Gates item in gate)
+            {
+                cmbxGate.Items.Add(item.Name);
+            }
+        }
+        private void fillRunway()
+        {
+            List<Runways> runway = db.Runways.ToList();
+            foreach (Runways item in runway)
+            {
+                cmbxRunway.Items.Add(item.Name);
+            }
+        }
+
+
+    }
+}
